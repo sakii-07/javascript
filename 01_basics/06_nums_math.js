f46c0266cb7867363eac876bef391323da800076
sakii-07 <sakshijagtap9304@gmail.com>
2025-10-19 15:46:35 +0000

    basics

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,16 @@
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
+
+
